1e62fb8aa24dfc4570beea0e6927467539edade9
nikotheengineer <fatsersvg@gmail.com>
2025-05-19 10:57:02 +0000

    Add new File

---
+++ practice_tasks/variables_data_types.py
@@ -0,0 +1,18 @@
+# How to create and use variables in python?
+# For a better explanation you can visit the w3school which I'm using as resource
+#https://www.w3schools.com/python/python_variables.asp
+
+
+##First of all variables are nothing without values so we need at assing a value to our variable
+
+Sensor1 = 'humidity'
+Sensor2 = 'motion'
+Sensor3 = 'temperature'
+
+#Since I'm trying too build a zoo sensor system my variables will be related to that.
+#Now we created values for our variables and when we print them--
+print(Sensor1)
+print(Sensor2)
+print(Sensor3)
+
+#We are seeing their values.
