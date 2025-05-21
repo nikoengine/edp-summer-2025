19a598f57d22064da1a9e69bb52dab09a16c3cd5
nikotheengineer <fatsersvg@gmail.com>
2025-05-21 16:30:17 +0000

    Add new File

--- practice_tasks/variables_data_types.py
+++ practice_tasks/variables_data_types.py
@@ -30,3 +30,65 @@ print(type(number))
 
 # Thats an integer! Well have you seen the difference between strings and integers.
 # If you didn't we put the strings between ('') these little guys. If you want to add a strings to your variable you need to make sure you have pair of those.
+
+#So what is a string?
+#A string is just text. Anything between quotes (' ' or " ") is considered a string.
+
+#You can do a lot with strings. Let me show you some cool string methods Python gives us.
+
+animal = 'Elephant' #Here we create another variable and it's an animal for our zoo.
+
+print(animal.upper())   # makes all letters uppercase
+print(animal.lower())   # makes all letters lowercase
+print(animal.startswith("E"))  # checks if the string starts with 'E'
+print(len(animal))  # gives us the length of the string 
+
+#There are bunch of other methods check it from here https://www.w3schools.com/python/python_strings.asp
+
+#So everythings cool but as we spoke about integer before there are 2 more data types would work for you. 
+#But what is a data? Data types tell Python what kind of data we’re working with.
+#Is it a number? Text? True/False? Python needs to know to handle it correctly.
+
+#int	 5, 100	        Whole numbers (integers)
+#float	 3.14, 0.01	    Decimal numbers (floating point)
+#str	 "hello", 'a'	Text (strings)
+#bool	 True, False	    True or False values
+
+
+age = 25             # int
+temperature = 36.6   # float
+name = "Serhat"      # str
+is_open = True       # bool
+
+#So when we check their types-->
+
+print(type(age))
+print(type(temperature))
+print(type(name))
+print(type(is_open))
+
+#We see their types. So yes it is pretty much like that. If you want to learn more go here https://www.w3schools.com/python/python_datatypes.asp
+
+
+#Well you see there are many animals in a zoo. But we cant just create another line for each of them. I mean you can if you want to but python giving us a convenience for that matter.
+# And thats called LIST's. Check this out.
+
+
+animals = ["Elephant", "Tiger", "Giraffe"]
+
+#Lists are surrounded by square brackets [] and can store multiple values.
+
+#We can:
+#Access items: 
+animals[0] # As in a set the first elements always be 0 and continiues from there. So we got to suit to that.
+
+#Add new ones: 
+animals.append("Lion")
+
+#Remove one : 
+animals.remove("Tiger")
+
+#Count them: 
+print(len(animals))
+
+print(animals)#By the way you have to do the changes before printing the list other ways it wont work. For example if we would have type len command before removing the tiger to output would be 4
