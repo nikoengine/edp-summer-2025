cef5c0ae0cf12f1c2ee20c9ecd25f5958ad08b2c
nikotheengineer <fatsersvg@gmail.com>
2025-05-26 11:14:00 +0000

    Add new File

---
+++ practice_tasks/python_loops.py
@@ -0,0 +1,14 @@
+#Well we all forgot to great our employees to their new job.
+#Lets do that. Meanwhile we are going to learn another feature of python.
+#Well there are many ways in python for using loops. We can either use dictionaires or lists.
+#But we need to use square brackets both of them.
+#First we are going to great our animals because we create them with lists.
+animals = ["elephant","tiger","lion","monkey"]
+
+for animal in animals:
+    print(f"Hello {animal}")
+
+#So you might get confused but don't worry.
+#We wrote animal because we wanted to use this instead of animals.
+#You see its something you need to do when you use loops.Something is need to represent your variable, list and dictionary.
+#It can be anything most of the coders use "i" or "x" but its doesnt really matter.
