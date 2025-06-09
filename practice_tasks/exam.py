7fe858a441dc55113385d66268e560158e3f622e
nikotheengineer <fatsersvg@gmail.com>
2025-06-09 11:32:59 +0000

    Add new File

--- practice_tasks/exam.py
+++ practice_tasks/exam.py
@@ -1,72 +1,90 @@
-#creating edp event driven system to manage zoo sensor system
+#If we are going to build a sensor system first we need channels to communicate with them
 from enum import Enum
-#If we are going to build a sensor system first we need channels.
-#like security, cage quality and animal confitions.
 
 security_channel = []
 cage_quality_channel = []
 
-#Now we need our sensor types to create sensor later.
-#We can add humidity sensor temperature sensor and motion sensor
-
+#Here we define our types for our events and sensors
 class SensorType(Enum):
     HUMIDITY = 'humidity'
-    TEMPERATURE = "temperature"
-    MOTION = "motion"
-
-#And we need our events related to our sensor types.
+    TEMPERATURE = 'temperature'
+    MOTION = 'motion'
 
 class EventType(Enum):
     HUMIDITY_ALERT = 'humidity_alert'
     TEMPERATURE_ALERT = 'temperature_alert'
     ANIMAL_ESCAPE = 'animal_escape'
 
-#Lets define our events.
-
+# Event class for define events
 class Event:
-    def __init__(self, name:EventType, payload):
+    def __init__(self, name: EventType, payload):
         self.name = name
         self.payload = payload
 
-#And next our sensor and what they to when they emitted an event
+#And defining sensors and what they do
 class Sensor:
-    def __init__(self,id_number, sensortype : SensorType, cage):
-        self.cage = cage
+    def __init__(self, id_number, sensortype: SensorType, cage):
         self.id_number = id_number
         self.sensortype = sensortype
+        self.cage = cage
 
-    def inform_event(self, name:EventType, payload):
+    def inform_event(self, name: EventType, payload):
         event = Event(name, payload)
-        if name == EventType.HUMIDITY_ALERT and EventType.TEMPERATURE_ALERT:
-            cage_quality_channel.append()
+        
+        
+        if name == EventType.HUMIDITY_ALERT or name == EventType.TEMPERATURE_ALERT:
+            cage_quality_channel.append(event)
         elif name == EventType.ANIMAL_ESCAPE:
-            security_channel.append()
+            security_channel.append(event)
         else:
             print("Unknown event!")
-        print(f"Sensor with payload {payload} informing event: {name.value} ")
+        
+        print(f"Sensor {self.id_number} ({self.sensortype.value}) from cage '{self.cage}' reporting event: {name.value} with payload {payload}")
 
-#now lets create our events and sensors.
-
+#Now lets create our events and sensors.
 temperature_sensor = Sensor(1, SensorType.TEMPERATURE, "elephants_cage")
 humidity_sensor = Sensor(2, SensorType.HUMIDITY, "tigers_cage")
 motion_sensor = Sensor(3, SensorType.MOTION, "lions_cage")
 
-#Now our events
 
-humidity_sensor.inform_event(EventType.HUMIDITY_ALERT, {"value": "%90", "cage_2":"tigers_cage"})
-temperature_sensor.inform_event(EventType.TEMPERATURE_ALERT, {"value": "40", "cage_1":"elephants_cage"})
-motion_sensor.inform_event(EventType.ANIMAL_ESCAPE{"cage_3":"monkeys_cage"})
+humidity_sensor.inform_event(EventType.HUMIDITY_ALERT, {"value": "%90", "cage": "tigers_cage"})
+temperature_sensor.inform_event(EventType.TEMPERATURE_ALERT, {"value": "40°C", "cage": "elephants_cage"})
+motion_sensor.inform_event(EventType.ANIMAL_ESCAPE, {"cage": "monkeys_cage"})
 
+#In a zoo we need 
+class Employee:
+    def __init__(self, id_number, name, role):
+        self.id_number = id_number
+        self.name = name
+        self.role = role
 
+    def handle_security_event(self):
+        if self.role == 'guard':
+            print(f"Guard {self.name} is catching the animal!")
 
+    def handle_cage_quality_event(self, event_name):
+        if self.role == 'zookeeper' and event_name == EventType.TEMPERATURE_ALERT:
+            print(f"Zookeeper {self.name} is stabilizing the temperature.")
+        elif self.role == 'cleaner' and event_name == EventType.HUMIDITY_ALERT:
+            print(f"Cleaner {self.name} is cleaning the cage due to high humidity.")
 
+employees = [
+    Employee(1, "Ali", "guard"),
+    Employee(2, "Tom", "cleaner"),
+    Employee(3, "Serhat", "zookeeper")
+]
 
-
-
+def dispatch_events():
+    print("\n---- DISPATCHING EVENTS ----")
     
-    
+    for event in security_channel:
+        print(f"[Dispatch] Security Event: {event.name.value}, payload: {event.payload}")
+        for emp in employees:
+            emp.handle_security_event()
 
-    
+    for event in cage_quality_channel:
+        print(f"\n[Dispatch] Cage Quality Event: {event.name.value}, payload: {event.payload}")
+        for emp in employees:
+            emp.handle_cage_quality_event(event.name)
 
-        
-    
+dispatch_events()
